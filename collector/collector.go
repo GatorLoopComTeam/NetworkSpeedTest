3f723acae13c4eae6c7f0292c81e6113be18b593
gdscheele <gdscheele@gmail.com>
2016-06-26 22:42:55 +0000

    added sender code

---
+++ collector/collector.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net"
+	"fmt"
+	"bufio"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "192.168.1.101:30000")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+	s := 0
+	r := 0
+	for {
+		fmt.Fprintf(conn, "250.5000\n")
+		s = s + 1
+		fmt.Println("Packets Sent: ", s)
+		message, err := bufio.NewReader(conn).ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		if message == "ack\n" {
+			r = r + 1
+			fmt.Println("Packets Received: ", r)
+		}
+	}
+}
